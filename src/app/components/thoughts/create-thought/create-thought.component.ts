5201b517202cd2ddde85d665690264001159ca1d
F8367918 Rafael Luiz Pereira <rafael.pereira.918@bb.com.br>
2024-03-11 14:46:13 -0300

    property binding/ two-way data binding/ event binding

--- src/app/components/thoughts/create-thought/create-thought.component.ts
+++ src/app/components/thoughts/create-thought/create-thought.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateThoughtComponent implements OnInit {
 
+  thought = {
+    id: 1,
+    content: 'some random thought',
+    author: 'some random author',
+    model: 'modelo1'
+  };
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  test() {
+    alert('123-test');
+  }
+
+  submit(e: Event) {
+    e.preventDefault();
   }
 
 }
